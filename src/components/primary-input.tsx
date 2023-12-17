f482c22fb41fd1a8f499e69e7eab3bded1ac730d
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-17 01:41:55 -0300

    Criando componente Header

---
+++ src/components/primary-input.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const PrimaryInput = styled.input`
+width: 352px;
+border-radius: 8px;
+background: var(--bg-secondary);
+padding: 10px 16px;
+border: none;
+
+font-family: inherit;
+font-weight: 400;
+font-size: 14px;
+line-height: 22px;
+color: var(--text-dark);
+`
